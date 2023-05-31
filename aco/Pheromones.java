999f2453b1d70a30a93585fb2035811eeec54651
agrevin <vinagre@artixvinagre.localdomain>
2023-05-31 17:05:38 +0100

    added Pheromone class with constructor

---
+++ aco/Pheromones.java
@@ -0,0 +1,19 @@
+package aco;
+
+public class 
+Pheromones 
+{
+    float eta, rho, pheromone_level;
+
+    public 
+    Pheromones( float _eta,
+                float _rho,
+                float _pheromone_level)
+    {
+        this.eta = _eta;
+        this.rho = _rho;
+        this.pheromone_level = _pheromone_level;
+    }
+
+
+}
