8f75235bf7b949f46383fd514428b945f5a265f6
agrevin <vinagre@artixvinagre.localdomain>
2023-06-01 20:00:23 +0100

    added methods to AcoEdge and changed variables to double

--- aco/AcoEdge.java
+++ aco/AcoEdge.java
@@ -1,9 +1,40 @@
 package aco;
 
+import java.lang.Math;
+
 public class AcoEdge extends Edge 
 {
-        public AcoEdge (float _weight, int _node0, int _node1)
-        {
-            super(_weight, _node0, _node1);
-        }
+    double pheromone_level = 0;
+    double eta;
+    double rho;
+    int events_since_evaporation = 0;
+    
+
+    public AcoEdge (double _weight, int _node0, int _node1, double _eta, double _rho)
+    {
+        super(_weight, _node0, _node1);
+        this.eta = _eta;
+        this.rho = _rho;
+    }
+
+    public double getPheromone_level()
+    {
+        return pheromone_level;
+    }
+
+    public void layPheromone(double new_pheromone) 
+    {
+        this.pheromone_level += new_pheromone;
+    }
+
+    public void evaporation(double random)
+    {
+        double probability;
+
+        this.events_since_evaporation++;
+        probability = 1 - Math.exp(0-(events_since_evaporation/this.eta));
+        
+        if (random < probability) this.pheromone_level -= this.rho;
+
+    }
 }
