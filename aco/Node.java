0916815b14742b1d589325ad3f312aa323e5cd3e
agrevin <vinagre@artixvinagre.localdomain>
2023-06-01 20:09:21 +0100

    Node constructor now works

--- aco/Node.java
+++ aco/Node.java
@@ -4,11 +4,12 @@ public class Node
 {
     int number;
     int edge0, edge1;
-    public _nodes(int _number, int _edge0, int _edge1)
+
+    public Node(int _number, int _edge0, int _edge1)
     {
-    this.edge0 = _edge0;
-    this.edge1 = _edge1;
-    this.number = _number;
+        this.edge0 = _edge0;
+        this.edge1 = _edge1;
+        this.number = _number;
     }
 
 }
