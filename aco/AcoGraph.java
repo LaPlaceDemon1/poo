25bb0885ac3ff183ae1f450d79e6d8f16dd39500
migui <miguel.a.rocha@tecnico.ulisboa.pt>
2023-06-01 19:38:29 +0100

    ~first

---
+++ aco/AcoGraph.java
@@ -0,0 +1,11 @@
+package aco;
+
+public class AcoGraph extends Graph {
+    int nest_node;
+    AcoGraph(int _node_num,int _nest_node) {
+
+        super(_node_num);
+        this.nest_node=_nest_node;
+
+    }
+}
