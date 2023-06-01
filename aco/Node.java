b3f0b27af962d6b4eadf17a922e465bbc3071ca1
Epilif <56264310+Epilif01@users.noreply.github.com>
2023-06-01 19:34:06 +0100

    Update Node.java

--- aco/Node.java
+++ aco/Node.java
@@ -6,8 +6,8 @@ public class Node
     int edge0, edge1;
     public _nodes(int _number, int _node0, int _node1)
     {
-    this.node0 = _node0;
-    this.node1 = _node1;
+    this.edge0 = _edge0;
+    this.edge1 = _edge1;
     this.number = _number;
     }
 
