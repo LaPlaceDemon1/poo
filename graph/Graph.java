a41d4521c6eb42f97dc1bea78fff89ce78c76c50
migui <miguel.a.rocha@tecnico.ulisboa.pt>
2023-06-02 14:56:20 +0100

    aaaahhhhh

--- graph/Graph.java
+++ graph/Graph.java
@@ -2,6 +2,10 @@ package aco;
 
 import graph.Node;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Graph {
     static int [][]edge_matrix;
     int node_number;
@@ -12,6 +16,18 @@ public class Graph {
         this.edge_matrix= new int [_node_num][_node_num];
 
     }
+    int[][] buildGraph(String file_name) {
+        String file_path="../maps"+file_name;
+        try (BufferedReader reader =new BufferedReader(new FileReader(file_path))){
+            String line;
+            while ((line = reader.readLine()) != null) {
 
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 
 }
