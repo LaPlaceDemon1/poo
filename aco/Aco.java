f140842e855a4f84daa5caae9a8dc1fc5b50d919
migui <miguel.a.rocha@tecnico.ulisboa.pt>
2023-06-03 23:00:18 +0100

    reader shoul be done

--- aco/Aco.java
+++ aco/Aco.java
@@ -1,4 +1,6 @@
 package aco;
 
+
+//definir o numero das vareaveis dos parametros como o nome dos parametros
 public class Aco {
 }
--- graph/Graph.java
+++ graph/Graph.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class Graph {
     int [][]edge_matrix;
     int node_number;
@@ -14,20 +15,32 @@ public class Graph {
         this.edge_matrix= new int [_node_num][_node_num];
 
     }
-    void buildGraph(String file_name) {
+    double [] buildGraph(String file_name) {
         String file_path="../maps"+file_name;
         String[] strings;
+        double[] parameters = new double[10];
         try (BufferedReader reader =new BufferedReader(new FileReader(file_path))){
             String line;
-            while ((line = reader.readLine()) != null) {
+            if ((line = reader.readLine()) != null) {
                 strings = line.split("\t");
-                boobs
+                for(int i =0; i<10 ; i++ ){
+                    parameters[9] = Double.parseDouble(line);
+                }
+            }
+            int j=0;
+            while ((line = reader.readLine()) != null) {
+
+                strings = line.split("\t");
+
+                for (int i=0;i<parameters[0];i++){
+                    this.edge_matrix[j][i]=Integer.parseInt(strings[i]);
+                }
                 System.out.println(line);
             }
         } catch (IOException e){
             e.printStackTrace();
         }
-
+        return parameters;
     }
 
 }
