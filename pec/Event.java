bdcf7110aa0109d450efc612fcb6523c0c078964
migui <miguel.a.rocha@tecnico.ulisboa.pt>
2023-06-04 18:11:21 +0100

    event reform

--- pec/Event.java
+++ pec/Event.java
@@ -1,11 +1,18 @@
 package pec;
  abstract class Event implements IEvent{
-    int time;
+    double time;
 
      public double getTime(){
         return this.time;
      };
 
+     public void setTime(double _time){
+         this.time=_time;
+     }
+
+     public void execute(){
+         return ;
+     }
 
      public int compareTo(Event that) {
          return Double.compare(this.time,that.time);
