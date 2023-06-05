4a95db549457b06e3455a7fd7f1ea9071b8237e0
agrevin <vinagre@artixvinagre.localdomain>
2023-06-05 11:50:14 +0100

    Pheromones now has values in double

--- aco/Pheromones.java
+++ aco/Pheromones.java
@@ -4,24 +4,23 @@ import java.util.Map;
 import graph.*;
 public class Pheromones 
 {
-    float eta, rho, pheromone_level;
-    Map<Edge, Float> pheromone_map;
+    double eta, rho;
+    Map<Edge, Double> pheromone_map;
 
-    Pheromones(float _eta, float _rho, float _pheromone_level)
+    Pheromones(double _eta, double _rho)
     {
         this.eta = _eta;
         this.rho = _rho;
-        this.pheromone_level = _pheromone_level;
     }
 
     //evaporation event: the given edge has its correspnding pheromone level from pheromone_map reduced by rho, if the pheromone level is less than 0, it is set to 0
     //and the method returns false, otherwise it returns true
     boolean evaporation(Edge edge)
     {
-        float new_pheromone_level = this.pheromone_map.get(edge) - this.rho;
+        double new_pheromone_level = this.pheromone_map.get(edge) - this.rho;
         if (new_pheromone_level < 0)
         {
-            this.pheromone_map.put(edge, 0.0f);
+            this.pheromone_map.put(edge, 0.0d);
             return false;
         }
         else
@@ -32,14 +31,14 @@ public class Pheromones
     }
     
     //lay pheromone event: the given edge has its corresponding pheromone level from pheromone_map increased by pheromone_level
-    void lay_pheromone(Edge edge)
+    void lay_pheromone(Edge edge, double pheromone_level)
     {
-        float new_pheromone_level = this.pheromone_map.get(edge) + this.pheromone_level;
+        double new_pheromone_level = this.pheromone_map.get(edge) + pheromone_level;
         this.pheromone_map.put(edge, new_pheromone_level);
     }
 
     //get the pheromone level of the given edge
-    float get_pheromone_level(Edge edge)
+    double get_pheromone_level(Edge edge)
     {
         return this.pheromone_map.get(edge);
     }
