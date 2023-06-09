0821d2152da40f72f6f73ad20a15b47dbf12a876
agrevin <vinagre@artixvinagre.localdomain>
2023-06-09 14:00:38 +0100

    Edge now, doesnt allow to use getNext from wrong Node

--- graph/Edge.java
+++ graph/Edge.java
@@ -1,25 +1,27 @@
 package graph;
 
-public class Edge
-{
+public class Edge {
     double weight;
     Node node0, node1;
 
-    public Edge (double _weight, Node _node0, Node _node1)
-    {
+    public Edge(double _weight, Node _node0, Node _node1) {
         this.weight = _weight;
         this.node0 = _node0;
         this.node1 = _node1;
     }
 
-    public double getWeight() 
-    {
+    public double getWeight() {
         return weight;
     }
 
-    public Node getNext(Node currentNode) 
-    {
-        if (currentNode.getId() == node1.getId()) return node0;
-        else return node1;
+    public Node getNext(Node currentNode) {
+        if (currentNode.getId() == node1.getId())
+            return node0;
+        else if (currentNode.getId() == node0.getId())
+            return node1;
+        else {
+            System.out.println("Error: Edge.getNext() called with wrong node");
+            return null;
+        }
     }
 }
