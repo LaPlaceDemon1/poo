5d9ad3ad5625c533220ccfe1c4ef11dc9eaf4de4
agrevin <vinagre@artixvinagre.localdomain>
2023-06-11 22:19:52 +0100

    not much rly

--- Client.java
+++ Client.java
@@ -2,30 +2,33 @@ import aco.*;
 import graph.*;
 
 public class Client {
-    public static void main(String args[]) {
-        Graph main_graph = new Graph();
-        if (args.length == 0) {
-            // error
-        }
-        int i = 0;
-        double[] parameters = new double[10];
+  public static void main(String args[]) {
+    Graph main_graph = new Graph();
+    if (args.length == 0) {
+      // error
+    }
+    int i = 0;
+    double[] parameters = new double[10];
 
-        // decide if input arguments have flag -r or -f``j
-        if (args[0].equals("-r")) {
-            // read from command line
-            for (i = 1; i < args.length; i++) {
-                parameters[i - 1] = Double.parseDouble(args[i]);
-            }
-        } else if (args[0].equals("-f")) {
+    // decide if input arguments have flag -r or -f``j
+    if (args[0].equals("-r")) {
+      // read from command line
+      for (i = 1; i < args.length; i++) {
+        parameters[i - 1] = Double.parseDouble(args[i]);
+      }
+    } else if (args[0].equals("-f")) {
 
-            double[] parametres = main_graph.buildGraph(args[1]);
-            // read from .txt file
-        } else {
-            // error
-        }
+      double[] parametres = main_graph.buildGraph(args[1]);
+      // read from .txt file
+    } else {
+      // error
+    }
 
-        Aco ant_colony = new Aco(parameters[0], parameters[1], parameters[3], parameters[4],
-                parameters[5], parameters[6], parameters[7], parameters[8]);
+    Aco ant_colony = new Aco(parameters[0], parameters[1], parameters[3],
+                             parameters[4], parameters[5], parameters[6],
+                             parameters[7], parameters[8], parameters[9]);
+
+        Pheromones pheromones = new Peromones(ant_colony.eta, ant_colony.rho, ant_colony.pheromone_level, //TODO List<Edges>)
         // if -f, read from .txt file
 
         // if -r, read from command line
@@ -33,5 +36,5 @@ public class Client {
         // read arguments from .txt file
 
         // read arguments from command line
-    }
+  }
 }
--- pec/Event.java
+++ pec/Event.java
@@ -2,22 +2,22 @@ package pec;
 
 import aco.Pheromones;
 
-abstract class Event implements IEvent{
-    double time;
+abstract class Event implements IEvent {
+ double time;
 
-     public double getTime(){
-        return this.time;
-     };
+ public double getTime() {
+  return this.time;
+ };
 
-     public void setTime(double _time){
-         this.time=_time;
-     }
-
-     public void execute(Object o){
-         return ;
-     }
-
-     public int compareTo(Event that) {
-         return Double.compare(this.time,that.time);
-     }
+ public void setTime(double _time) {
+  this.time = _time;
  }
+
+ public void execute(Object o) {
+  return;
+ }
+
+ public int compareTo(Event that) {
+  return Double.compare(this.time, that.time);
+ }
+}
