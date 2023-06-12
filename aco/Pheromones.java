40f6ac3d0e5a206c4ca3864524ab8c0924e73571
agrevin <vinagre@artixvinagre.localdomain>
2023-06-12 14:50:24 +0100

    generate evaporate event once lay_pheromones is called

--- aco/Pheromones.java
+++ aco/Pheromones.java
@@ -1,62 +1,62 @@
 package aco;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import graph.*;
-public class Pheromones 
-{
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+import pec.*;
+
+public class Pheromones {
     double eta, rho, pheromone_level;
     Map<Edge, Double> pheromone_map;
 
-    public Pheromones(double _eta, double _rho, double _pheromone_level, List<Edge> edges)
-    {
+    public Pheromones(double _eta, double _rho, double _pheromone_level,
+            ArrayList<Edge> edges) {
         this.eta = _eta;
         this.rho = _rho;
         this.pheromone_level = _pheromone_level;
-        for (Edge edge : edges)
-        {
+        for (Edge edge : edges) {
             this.pheromone_map.put(edge, 0.0d);
         }
     }
 
-    //evaporation event: the given edge has its correspnding pheromone level from pheromone_map reduced by rho, if the pheromone level is less than 0, it is set to 0
-    //and the method returns false, otherwise it returns true
-    public boolean evaporation(Edge edge)
-    {
+    // evaporation event: the given edge has its correspnding pheromone level from
+    // pheromone_map reduced by rho, if the pheromone level is less than 0, it is
+    // set to 0 and the method returns false, otherwise it returns true
+    public boolean evaporation(Edge edge) {
         double new_pheromone_level = this.pheromone_map.get(edge) - this.rho;
-        if (new_pheromone_level < 0)
-        {
+        if (new_pheromone_level < 0) {
             this.pheromone_map.put(edge, 0.0d);
             return false;
-        }
-        else
-        {
+        } else {
             this.pheromone_map.put(edge, new_pheromone_level);
             return true;
         }
     }
-    
-    //lay pheromone event: the given edge has its corresponding pheromone level from pheromone_map increased by pheromone_level
-    void lay_pheromone(ArrayList<Edge> edges) 
-    {
+
+    // lay pheromone event: the given edge has its corresponding pheromone level
+    // from pheromone_map increased by pheromone_level
+    void lay_pheromone(ArrayList<Edge> edges, EventList sim, double time) {
+        Random random = new Random();
         double total_weight = 0;
-        for (Edge edge : edges)
-        {   
-            if (this.pheromone_map.get(edge) == 0); //TODO generate event;
+        for (Edge edge : edges) {
+            if (this.pheromone_map.get(edge) == 0)
+                ;
+            sim.add(
+                    new Evaporate(time + -this.eta * Math.log(1 - random.nextDouble()),
+                            this, edge, this.eta));
+
             total_weight += edge.getWeight();
         }
-        for (Edge edge : edges)
-        {
-            double new_pheromone_level = this.pheromone_map.get(edge) + ((this.pheromone_level*edge.getWeight())/total_weight);
+        for (Edge edge : edges) {
+            double new_pheromone_level = this.pheromone_map.get(edge) +
+                    ((this.pheromone_level * edge.getWeight()) / total_weight);
             this.pheromone_map.put(edge, new_pheromone_level);
         }
     }
 
-    //get the pheromone level of the given edge
-    double get_pheromone_level(Edge edge)
-    {
+    // get the pheromone level of the given edge
+    double get_pheromone_level(Edge edge) {
         return this.pheromone_map.get(edge);
     }
-
 }
