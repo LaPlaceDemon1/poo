4b57c0eeddc3955502eb09a916bd47afcd25d647
agrevin <vinagre@artixvinagre.localdomain>
2023-06-12 16:21:01 +0100

    some getters ACO

--- aco/Aco.java
+++ aco/Aco.java
@@ -1,13 +1,17 @@
 package aco;
 
+import java.util.PriorityQueue;
+
 // definir o numero das vareaveis dos parametros como o nome dos parametros
 public class Aco {
     public double num_nodes, nest_node, colony_size;
-    public double alpha, beta, delta, eta, rho, pheromone_level;
+    public double alpha, beta, delta, eta, rho, pheromone_level, final_time;
+    public double num_movs, num_evap;
+    public PriorityQueue<Cycle> cycles;
 
     public Aco(double _num_nodes, double _nest_node, double _alpha, double _beta,
             double _delta, double _eta, double _rho, double _pheromone_level,
-            double _colony_size) {
+            double _colony_size, double _final_time) {
         this.num_nodes = _num_nodes;
         this.nest_node = _nest_node;
         this.colony_size = _colony_size;
@@ -17,5 +21,23 @@ public class Aco {
         this.eta = _eta;
         this.rho = _rho;
         this.pheromone_level = _pheromone_level;
+        this.final_time = _final_time;
+        this.cycles = new PriorityQueue<Cycle>();
+    }
+
+    public double getNumNodes() {
+        return this.num_nodes;
+    }
+
+    public double getNestNode() {
+        return this.nest_node;
+    }
+
+    public double getColonySize() {
+        return this.colony_size;
+    }
+
+    public double getAlpha() {
+        return this.alpha;
     }
 }
