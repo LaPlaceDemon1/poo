27e854c0aab31b5f09de96c436a8517bd59e298e
agrevin <vinagre@artixvinagre.localdomain>
2023-06-12 17:23:44 +0100

    Cycle class with print cycle

--- aco/Cycle.java
+++ aco/Cycle.java
@@ -1,4 +1,22 @@
 package aco;
 
 public class Cycle {
+  int[] nodes;
+  double cost;
+
+  public Cycle(int[] _nodes, double _cost) {
+    this.nodes = _nodes;
+    this.cost = _cost;
+  }
+
+  public void printCycle() {
+    System.out.print("{");
+    for (int i = 0; i < this.nodes.length; i++) {
+      System.out.print(this.nodes[i]);
+      if (i < this.nodes.length - 1) {
+        System.out.print(", ");
+      }
+      System.out.println("}: " + this.cost);
+    }
+  }
 }
